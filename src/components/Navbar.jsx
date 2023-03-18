6cd95595da096a5b8098a679c71b981d6c586bd0
Tinsae Ayana <tinsaeaeroastro@gmail.com>
2023-03-18 10:21:28 +0300

    upgrade

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,19 +36,19 @@ const Navbar = () => {
             >
                <hr className='duration-700 lg:hidden block mt-5'></hr>
                <div className='flex  lg:flex-row flex-col justify-evenly w-1/2 lg:px-2 px-0 space-y-7 lg:space-y-0' onClick={showNav}>
-                  <NavLink to='/' className={({ isActive }) => (isActive ? activeDec : notActiveDec)}>
+                  <NavLink to='' className={({ isActive }) => (isActive ? activeDec : notActiveDec)}>
                      Overview
                   </NavLink>
-                  <NavLink to='/Example' className={({ isActive }) => (isActive ? activeDec : notActiveDec)}>
+                  <NavLink to='Example' className={({ isActive }) => (isActive ? activeDec : notActiveDec)}>
                      Examples
                   </NavLink>
-                  <NavLink to='/Tour' className={({ isActive }) => (isActive ? activeDec : notActiveDec)}>
+                  <NavLink to='Tour' className={({ isActive }) => (isActive ? activeDec : notActiveDec)}>
                      Tour
                   </NavLink>
-                  <NavLink to='/Blog' className={({ isActive }) => (isActive ? activeDec : notActiveDec)}>
+                  <NavLink to='Blog' className={({ isActive }) => (isActive ? activeDec : notActiveDec)}>
                      Blog
                   </NavLink>
-                  <NavLink to='/Help' className={({ isActive }) => (isActive ? activeDec : notActiveDec)}>
+                  <NavLink to='Help' className={({ isActive }) => (isActive ? activeDec : notActiveDec)}>
                      Help
                   </NavLink>
                </div>
