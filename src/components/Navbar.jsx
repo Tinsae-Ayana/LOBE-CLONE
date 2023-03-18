b4871b07deb9f3b8ca1ab0ab17065da4e46f3a5f
Tinsae Ayana <tinsaeaeroastro@gmail.com>
2023-03-18 10:35:32 +0300

    upgrade

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,10 +36,10 @@ const Navbar = () => {
             >
                <hr className='duration-700 lg:hidden block mt-5'></hr>
                <div className='flex  lg:flex-row flex-col justify-evenly w-1/2 lg:px-2 px-0 space-y-7 lg:space-y-0' onClick={showNav}>
-                  <NavLink to='/' className={({ isActive }) => (isActive ? activeDec : notActiveDec)}>
+                  <NavLink to='/LOBE-CLONE' className={({ isActive }) => (isActive ? activeDec : notActiveDec)}>
                      Overview
                   </NavLink>
-                  <NavLink to='Example' className={({ isActive }) => (isActive ? activeDec : notActiveDec)}>
+                  <NavLink to='/LOBE-CLONE/Example' className={({ isActive }) => (isActive ? activeDec : notActiveDec)}>
                      Examples
                   </NavLink>
                   <NavLink to='Tour' className={({ isActive }) => (isActive ? activeDec : notActiveDec)}>
